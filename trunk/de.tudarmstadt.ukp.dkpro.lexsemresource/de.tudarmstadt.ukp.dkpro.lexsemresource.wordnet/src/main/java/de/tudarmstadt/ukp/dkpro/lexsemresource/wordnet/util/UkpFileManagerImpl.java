926a733a89e97547da8fdfcc006d3b93fc620296
torsten.zesch <torsten.zesch@cebd3e36-6d11-3217-616d-616dcdf7f7c0>
2012-07-12 12:44:08 +0000

    Added license headers.

--- trunk/de.tudarmstadt.ukp.dkpro.lexsemresource/de.tudarmstadt.ukp.dkpro.lexsemresource.wordnet/src/main/java/de/tudarmstadt/ukp/dkpro/lexsemresource/wordnet/util/UkpFileManagerImpl.java
+++ trunk/de.tudarmstadt.ukp.dkpro.lexsemresource/de.tudarmstadt.ukp.dkpro.lexsemresource.wordnet/src/main/java/de/tudarmstadt/ukp/dkpro/lexsemresource/wordnet/util/UkpFileManagerImpl.java
@@ -1,7 +1,39 @@
-/**
- * Java WordNet Library (JWNL)
- * See the documentation for copyright information.
- */
+/*******************************************************************************
+ * Copyright 2012
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This code is based on the JWNL library licensed under the BSD license.
+ * In accordance with the terms of this license,
+ * the following copyright statement is retained:
+ *
+ * Copyright (C) 2000-2007 the JWNL development team (http://www.sourceforge.net/projects/jwordnet)
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided
+ * that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ *   following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ *   the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * - Neither the name of the product ("JWNL") nor the names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior written permission.
+ *
+ * Furthermore the modified code is re-licensed under the Apache License,
+ * Version 2.0 as stated above.
+ *****************************************************************************/
 package de.tudarmstadt.ukp.dkpro.lexsemresource.wordnet.util;
 
 import java.io.File;
